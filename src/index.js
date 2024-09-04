a8636eadc0d65e81254f288087e60a31813237ce
asepizal <aseprizal2496@gmail.com>
2024-09-04 16:00:46 +0700

    commit kedua

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ const navButton = document.querySelector('.nav-button')
 const navMenu = document.querySelector('.nav-menu')
 console.log("🚀 ~ navButton:", navButton)
 navButton.addEventListener('click',function(){
-    navMenu.classList.toggle('hidden')
+    navMenu.classList.toggle('h-screen-minus-header')
+    navMenu.classList.toggle('h-0')
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     extend: {
       fontFamily:{
         sora: ["Sora"],
+        onest: "Onest"
       },
+      backgroundColor:{
+        primary: "#0f0715"
+      },
+      backgroundImage:{
+        radial: "radial-gradient(circle at 50% 30%,#2a1454 -20%,#0f0715 85%)"
+      },
+      height: {
+        'screen-minus-header': 'calc(100vh - 95px)',
+      }
     },
   },
   plugins: [],
